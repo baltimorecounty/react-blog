769f51bc1aa30e59ae4de8333ee4a75b12fe2f27
Marty Powell <jpowel25@gmail.com>
2018-07-05 08:29:59 -0400

    pointed to proper file

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card';
+import Card from './Card.jsx';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
