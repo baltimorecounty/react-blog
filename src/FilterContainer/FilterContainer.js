54885b93754933f32b351c6ff15cc3f150a9dafd
Marty Powell <jpowel25@gmail.com>
2018-07-10 12:42:56 -0400

    added filter container css

--- src/FilterContainer/FilterContainer.js
+++ src/FilterContainer/FilterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MultipleSelectFilter from '../Filter/MultipleSelectFilter';
+import './FilterContainer.css';
 
 const propTypes = {
 	title: PropTypes.string,
