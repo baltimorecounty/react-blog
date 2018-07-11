61104e954addb036dbb0839efd6e616a47cf1e9c
Marty Powell <jpowel25@gmail.com>
2018-07-11 11:48:08 -0400

    pass in filters to the structured content list

--- src/index.js
+++ src/index.js
@@ -5,5 +5,37 @@ import StructureContentList from './StructuredContentList';
 import registerServiceWorker from './registerServiceWorker';
 import BlogCardContent from './Blog/BlogCardContent';
 
-ReactDOM.render(<StructureContentList cardContentComponent={BlogCardContent} />, document.getElementById('root'));
+const filters = [
+    {
+        title: 'Category',
+        type: 'single',
+        options: [
+            {
+                label: 'Show All Blog Categories',
+                value: 'all',
+                isCheckedByDefault: true
+            },
+            {
+                label: 'Collection and Materials',
+                value: 'collections-and-materials'
+            },
+            {
+                label: 'News',
+                value: 'news'
+            },
+            {
+                label: 'Programming and Events',
+                value: 'programming-and-events'
+            }
+        ]
+    }
+];
+
+ReactDOM.render(
+    <StructureContentList
+        filters={filters}
+        cardContentComponent={BlogCardContent}
+    />,
+    document.getElementById('root')
+);
 registerServiceWorker();
