a74be00e1721d6d8914277a02cd1905314856b15
Marty Powell <jpowel25@gmail.com>
2018-07-11 11:53:04 -0400

    update

---
+++ src/Blog/CardList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'baltimorecounty-react-components';
+import BlogCardContent from '../Blog/BlogCardContent';
+
+const propTypes = {
+    contentType: PropTypes.string,
+    contentItems: PropTypes.array
+};
+
+const defaultProps = {
+    contentItems: []
+};
+
+export default class CardList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                {this.props.contentItems.map(contentItem => {
+                    return (
+                        <Card key={contentItem.Id} cardType={contentType}>
+                            <BlogCardContent contentItem={contentItem} />
+                        </Card>
+                    );
+                })}
+            </React.Fragment>
+        );
+    }
+}
+
+CardList.propTypes = propTypes;
+CardList.defaultProps = defaultProps;
