2f78daa336e51476450eecc049d8d1211abbe288
Marty Powell <jpowel25@gmail.com>
2018-08-15 15:12:14 -0400

    safely check to see if we want to display filters

--- src/FilterContainer/FilterContainer.js
+++ src/FilterContainer/FilterContainer.js
@@ -4,9 +4,9 @@ import Filter from '../Filter/Filter';
 
 const propTypes = {
     title: PropTypes.string,
-	filters: PropTypes.array.isRequired,
-	onChange: PropTypes.func,
-	type: PropTypes.string
+    filters: PropTypes.array.isRequired,
+    onChange: PropTypes.func,
+    type: PropTypes.string
 };
 
 const defaultProps = {
@@ -16,21 +16,23 @@ const defaultProps = {
 
 export default class FilterContainer extends React.Component {
     render() {
-		const { title, filters, onChange } = this.props;
+        const { title, filters, onChange } = this.props;
 
         return (
             <React.Fragment>
-                <div className="bc_filters filters">
-                    <h3 className="filters-title">{title}</h3>
-                    {filters.map((filter, index) => (
-                        <Filter
-                            onChange={onChange}
-                            key={index}
-                            filter={filter}
-                        />
-                    ))}{' '}
-                    {/* We should be able to pass in another component? */}
-                </div>
+                {filters && filters.length && (
+                    <div className="bc_filters filters">
+                        <h3 className="filters-title">{title}</h3>
+                        {filters.map((filter, index) => (
+                            <Filter
+                                onChange={onChange}
+                                key={index}
+                                filter={filter}
+                            />
+                        ))}{' '}
+                        {/* We should be able to pass in another component? */}
+                    </div>
+                )}
             </React.Fragment>
         );
     }
