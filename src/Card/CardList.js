1063340bdaf7615b38e1bcc82ba97d3fa4f8acec
Marty Powell <jpowel25@gmail.com>
2018-08-20 11:36:07 -0400

    added fade animation to cards

--- src/Card/CardList.js
+++ src/Card/CardList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group'; // ES6
 import { Card } from 'baltimorecounty-react-components';
+import './CardList.css';
 
 const propTypes = {
     contentType: PropTypes.string,
@@ -13,19 +15,32 @@ const defaultProps = {
 
 export default class CardList extends React.Component {
     render() {
-        const { cardContentComponent: CardContentComponent } = this.props;
+		const { cardContentComponent: CardContentComponent } = this.props;
+		const cards = this.props.contentItems.map(contentItem => {
+			return (
+				<Card
+					key={contentItem.Id}
+					cardType={this.props.contentType}
+				>
+					<CardContentComponent
+						contentItem={contentItem}
+					/>
+				</Card>
+			);
+		});
+		const animationDelay = 250;
+
         return (
             <React.Fragment>
-                {this.props.contentItems.map(contentItem => {
-                    return (
-                        <Card
-                            key={contentItem.Id}
-                            cardType={this.props.contentType}
-                        >
-                            <CardContentComponent contentItem={contentItem} />
-                        </Card>
-                    );
-                })}
+                <CSSTransitionGroup
+                    transitionName="list-fade"
+                    transitionEnterTimeout={animationDelay}
+					transitionLeaveTimeout={animationDelay}
+					transitionAppear={true}
+					transitionAppearTimeout={animationDelay}
+                >
+                    {cards}
+                </CSSTransitionGroup>
             </React.Fragment>
         );
     }
