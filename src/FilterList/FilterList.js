e80e517afbde9678615e519dc7fb8035eaa74860
Marty Powell <jpowel25@gmail.com>
2018-08-23 09:27:26 -0400

    refactor

--- src/FilterList/FilterList.js
+++ src/FilterList/FilterList.js
@@ -4,54 +4,68 @@ import { Button, ButtonGroup } from 'react-bootstrap';
 import './FilterList.css';
 
 const propTypes = {
-	filterList: PropTypes.array.isRequired,
-	onChange: PropTypes.func.isRequired
+    filterList: PropTypes.array.isRequired,
+    onChange: PropTypes.func.isRequired
 };
 
 const defaultProps = {};
 
 export default class FilterList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeFilter: null
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeFilter: null
+        };
 
-		this.onButtonClick = this.onButtonClick.bind(this);
-		this.isActive = this.isActive.bind(this);
-	}
+        this.onButtonClick = this.onButtonClick.bind(this);
+        this.isActive = this.isActive.bind(this);
+    }
 
-	isActive(item) {
-		return this.state.activeFilter === item.name;
-	}
+    isActive(item) {
+        return this.state.activeFilter === item.name;
+    }
 
-	onButtonClick(clickEvent) {
-		const { currentTarget } = clickEvent;
+    onButtonClick(clickEvent) {
+        const { currentTarget } = clickEvent;
 
-		this.setState({
-			activeFilter: currentTarget.textContent.trim()
-		}, () => {
-			this.props.onChange(this.state.activeFilter);
-		});
-	}
+        this.setState(
+            {
+                activeFilter: currentTarget.textContent.trim()
+            },
+            () => {
+                this.props.onChange(this.state.activeFilter);
+            }
+        );
+    }
 
-	render() {
-		return (
-			<React.Fragment>
-				<ButtonGroup className="filter-group">
-					{this.props.filterList.map((item, itemIndex) =>
-						<h4>
-						<Button
-							key={itemIndex}
-							onClick={this.onButtonClick}
-							active={this.isActive(item)}>
-						{item.icon &&
-							<i className={`fa fa-icon ${item.icon}`}></i>} {item.name}</Button>)</h4>}
-				</ButtonGroup>
-			</React.Fragment>
-		);
-	}
+    render() {
+        const { filterList } = this.props;
+
+        const filters = filterList.map((item, itemIndex) => {
+            return (
+                <h4>
+                    <Button
+                        key={itemIndex}
+                        onClick={this.onButtonClick}
+                        active={this.isActive(item)}
+                    >
+                        {item.icon && (
+                            <i className={`fa fa-icon ${item.icon}`} />
+                        )}{' '}
+                        {item.name}
+                    </Button>
+                    )
+                </h4>
+            );
+        });
+
+        return (
+            <React.Fragment>
+                <ButtonGroup className="filter-group">{filters}</ButtonGroup>
+            </React.Fragment>
+        );
+    }
 }
 
- FilterList.propTypes = propTypes;
- FilterList.defaultProps = defaultProps;
+FilterList.propTypes = propTypes;
+FilterList.defaultProps = defaultProps;
